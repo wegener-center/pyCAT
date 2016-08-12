a2a858c5b3d9af8687d060a67c8a8379c1c31a25
Armin Leuprecht <armin.leuprecht@uni-graz.at>
2016-08-12 13:12:40 +0200

    added first test

---
+++ pycat/tests/esd/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# (C) Wegener Center for Climate and Global Change, University of Graz, 2015
+#
+# This file is part of pyCAT.
+#
+# pyCAT is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License version 3 as published by the
+# Free Software Foundation.
+#
+# pyCAT is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pyCAT. If not, see <http://www.gnu.org/licenses/>.
+"""
+Package for testing the pycat.esd package
+"""
